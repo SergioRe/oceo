6cacb1b02567b99d153e90bc1ed725b27c2eceb7
Sergio Renato Mendoza Pisconte <renato.mpisconte@gmail.com>
2016-10-17 15:32:55 -0500

    Agregando Menu Principal

--- application/views/inicio_view.php
+++ application/views/inicio_view.php
@@ -8,8 +8,37 @@
     <?php require_once(APPPATH."views/script-css/script-css.php"); ?>
     </head> 
 <body>
-    <div class="container">
-        <h1>OCEO S.A.C.</h1>
+    <div class="mainWrap">
+        <a id="touch-menu" class="mobile-menu" href="#">
+            <span class="glyphicon glyphicon-align-justify"></span>&nbsp;&nbsp;Menu
+        </a>
+        <nav>
+            <ul class="menu">
+                <li class="movil">
+                    <a href="#">
+                        Logo OCEO S.A.C.
+                    </a>
+                </li>
+                <li class="movil"><a href="#">Publicar</a></li>
+                <li class="movil"><a href="#">Nosotros</a></li>
+                <li class="movil"><a href="#">Blog</a></li>
+                <li class="movil"><a href="#">Servicios</a></li>
+                <li class="movil">
+                    <a href="#">
+                        <div id="custom-search-input">
+                            <div class="input-group col-md-12">
+                                <input type="text" class="form-control input-sm" placeholder="Buscar" />
+                                <span class="input-group-btn">
+                                    <button class="btn btn-info btn-sm" type="button">
+                                        <i class="glyphicon glyphicon-search"></i>
+                                    </button>
+                                </span>
+                            </div>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </body>
 </html>
--- application/views/script-css/script-css.php
+++ application/views/script-css/script-css.php
@@ -1,7 +1,10 @@
 <!--css-->
-<link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css')?>" rel="stylesheet">
 <link href="<?php echo base_url('assets/datatables/css/dataTables.bootstrap.css')?>" rel="stylesheet">
 <link href="<?php echo base_url('assets/bootstrap-datepicker/css/bootstrap-datepicker3.min.css')?>" rel="stylesheet">
+<link rel="stylesheet" href="<?php echo base_url('assets/bootstrap/estilos_1.css')?>" type="text/css" media="screen">
+<link rel="stylesheet" href="<?php echo base_url('assets/font-awesome/css/font-awesome.css')?>" >
+<link href="<?php echo base_url('assets/bootstrap/css/bootstrap-social.css')?>" rel="stylesheet">
 
 <!--JavaScript-->
 <script src="<?php echo base_url('assets/jquery/jquery-2.1.4.min.js')?>"></script>
@@ -9,3 +12,4 @@
 <script src="<?php echo base_url('assets/datatables/js/jquery.dataTables.min.js')?>"></script>
 <script src="<?php echo base_url('assets/datatables/js/dataTables.bootstrap.js')?>"></script>
 <script src="<?php echo base_url('assets/bootstrap-datepicker/js/bootstrap-datepicker.min.js')?>"></script>
+<script src="<?php echo base_url('assets/bootstrap/js/menu.js')?>" type="text/javascript"></script> 
